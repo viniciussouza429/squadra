b1da0db3f1f7c9c2cc7d2c30cfa2152198730c9e
viniciussouza429 <vinicius.souza429@hotmail.com>
2025-10-20 23:10:49 -0300

    fix: Resolvendo erro de export do Auth.js separando a config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,11 +2,7 @@
 
 import NextAuth from "next-auth";
 // 🎯 Importa APENAS o objeto de configuração do arquivo auxiliar
-import { authOptions } from "../auth-options";
-
-// 1. Cria o Handler principal do NextAuth
+import { authOptions } from "@/app/api/auth/auth-options";
 const handler = NextAuth(authOptions);
 
-// 2. 🎯 CORREÇÃO FINAL: Exporta o handler para GET e POST
-// Isso diz ao Next.js: "Use esta função para lidar com todas as requisições Auth"
 export { handler as GET, handler as POST };
